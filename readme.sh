e8641dfa4d9599d3bb6a82c2a0099d2948466fe8
Heng490 <chzsdau@163.com>
2025-09-26 09:04:57 +0800

    Delete readme.sh

--- readme.sh
+++
@@ -1,3 +0,0 @@
-# ================================================
-# Scripts developed and used in the article
-# "Genome-wide Sequencing Reveals Selective Signatures for Characteristic Traits and Breed-specific Tag SNP Detection in Chinese Gamecocks".
