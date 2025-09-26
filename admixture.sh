e5a5446b8203cd6cbf06110ea0cdc7c0c3c39ae3
Heng490 <chzsdau@163.com>
2025-09-26 09:04:38 +0800

    Delete admixture.sh

--- admixture.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Admixture analysis (estimating K from 2 to 6)
-# Running Admixture with cross-validation (cv) for different K values
-for k in {2..6}; do
-  admixture -j2 -C 0.01 --cv admixture.bed $k > admixture.log$k.out
-done
